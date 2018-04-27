4b492ad83aa6ef3f9be73c9cc99a82f2b580e4c4
Justus Sagemüller <sagemueller@geo.uni-koeln.de>
2018-04-27 12:33:41 +0200

    Introduce a type to properly encapsulate progress information.
    Goal: don't store this information in fragile chunks in a cookie, but
    in a proper URL.

---
+++ Presentation/Yeamer/Internal/Progress.hs
@@ -0,0 +1,60 @@
+-- |
+-- Module      : Presentation.Yeamer.Internal.Progress
+-- Copyright   : (c) Justus Sagemüller 2018
+-- License     : GPL v3
+-- 
+-- Maintainer  : (@) jsagemue $ uni-koeln.de
+-- Stability   : experimental
+-- Portability : portable
+-- 
+module Presentation.Yeamer.Internal.Progress where
+
+
+import Data.Map (Map)
+import qualified Data.Map as Map
+import qualified Data.Vector as Arr
+
+import Data.ByteString (ByteString)
+import Data.Text (Text)
+import qualified Data.Text.Encoding as Txt
+import qualified Data.ByteString.Base64.URL as URLBase64
+
+import Data.Flat (flat, unflat)
+
+import Yesod (PathPiece(..))
+
+import Control.Arrow ((>>>), (<<<))
+import Control.Monad ((>=>))
+import Control.Monad.Trans.List
+import Control.Monad.Trans.Writer
+import Lens.Micro (_Right)
+import Lens.Micro.Extras (preview)
+
+import Data.Traversable.Redundancy (rmRedundancy)
+
+
+newtype PresProgress = PresProgress
+    { getPresentationProgress :: Map [Text] ByteString }
+    deriving (Eq, Show, Read)
+
+instance PathPiece PresProgress where
+  fromPathPiece = Txt.encodeUtf8
+              >>> preview _Right . URLBase64.decode
+              >=> preview _Right . fmap assemblePresProgress . unflat
+  toPathPiece   = Txt.decodeUtf8
+              <<<                  URLBase64.encode
+              <<<                  flat . disassemblePresProgress
+
+assemblePresProgress :: ([Text], [ByteString], Map [Int] Int) -> PresProgress
+assemblePresProgress (pSR_l, pKR_l, prog_c)
+          = PresProgress . Map.mapKeys (map (progStepRsr Arr.!))
+                          $ fmap (progKeyRsr Arr.!) prog_c
+ where progStepRsr = Arr.fromList pSR_l
+       progKeyRsr = Arr.fromList pKR_l
+
+disassemblePresProgress :: PresProgress -> ([Text], [ByteString], Map [Int] Int)
+disassemblePresProgress (PresProgress progs)
+         = (Arr.toList progStepRsr, Arr.toList progKeyRsr, compressedProgs)
+ where (ListT (WriterT keyCompressed), progStepRsr)
+                  = rmRedundancy . ListT . WriterT $ Map.toList progs
+       (compressedProgs,progKeyRsr) = rmRedundancy $ Map.fromList keyCompressed
